60fbc63817aa1460284f6d39c6eb545428122b2b
marjimveg1 <maria-2696@hotmail.com>
2019-04-01 19:17:33 +0200

    iniciando casos de uso

---
+++ Acme-HackerRank/src/test/java/services/PositionServiceTest.java
@@ -0,0 +1,28 @@
+
+package services;
+
+import javax.transaction.Transactional;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/datasource.xml", "classpath:spring/config/packages.xml"
+})
+@Transactional
+public class PositionServiceTest extends AbstractTest {
+
+	// Service under testing -----------------------------------
+	@Autowired
+	private PositionService	positionService;
+
+	// Other services ------------------------------------------
+
+	// Suite test ---------------------------------------------
+
+}
